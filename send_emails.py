c45b18fd7e0b023f1235a3db285f0d49fc7ad0f6
Keshav Jha <keshavj215@gmail.com>
2024-08-06 14:07:18 +0530

    update

--- send_emails.py
+++ send_emails.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, supports_credentials=True)
 
 EMAIL_SERVER = "smtp.gmail.com"
 PORT = 587
